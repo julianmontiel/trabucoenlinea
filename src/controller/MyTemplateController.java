3966e33484eee38360effa05d0b85189f7649b9a
Julian Montiel <julian.montiel@grobanking.com>
2016-03-05 10:15:37 -0600

    Issue #3 Done. Se realizo la verison inicial de la estrucutra del proyecto

---
+++ src/controller/MyTemplateController.java
@@ -0,0 +1,38 @@
+package controller;
+
+/**
+ * Created by julian on 3/5/16.
+ */
+
+import javax.faces.bean.*;
+
+@ManagedBean(name="myTemplateController")
+@SessionScoped
+public class MyTemplateController {
+
+    private String title = "";
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String index(){
+        this.title = "Home";
+        return "index";
+    }
+
+    public String aboutus(){
+        this.title = "About Us";
+        return "aboutus";
+    }
+
+    public String news(){
+        this.title = "News";
+        return "news";
+    }
+
+}
